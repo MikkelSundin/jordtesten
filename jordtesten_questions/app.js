2b23cc40630d1336a3e6efc02ca7ea5693332515
MikkelSundin <mikkel.sundin@gmail.com>
2025-04-22 18:32:36 +0200

    Delete jordtesten_questions directory

--- jordtesten_questions/app.js
+++
@@ -1,7 +0,0 @@
-function startTest() {
-    // Tøm tidligere svar (valgfritt)
-    localStorage.clear();
-
-    // Gå videre til første spørsmålsside
-    window.location.href = "question1.html";
-}
--- jordtesten_questions/service-worker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', function(event) {
-  console.log('Service Worker installert.');
-});
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(fetch(event.request));
-});
